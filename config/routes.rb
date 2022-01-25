42fd52e1f81311e708992a45ab8f415ae6136b90
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-01-25 20:25:11 +0100

    routes correction for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index,:create,:new,:show] do
+  root to: 'lists#index'
+  resources :lists, only: [:create,:new,:show] do
     resources :bookmarks, only: [:index, :new, :create]
   end
 
